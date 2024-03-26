4c16d1cc119b1ad3b556f179bd80a8e6e8e13b0e
Aluno <Aluno@LAB202-PC10.ifmg.edu.br>
2024-03-26 10:23:16 -0300

    Adicionei o meu metodo

--- aula/src/main/java/aula/git/Apresentacao.java
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -4,9 +4,15 @@ public class Apresentacao {
 
     public void apresentar() {
         this.lucasSouza();
+        this.LucasMagalhaes();
     }
 
     private void lucasSouza() {
+        System.out.println("");
+        System.out.println("Estou colaborando com o git");
+    }
+
+    private void LucasMagalhaes() {
         System.out.println("");
         System.out.println("Estou colaborando com o git");
         System.out.println("Estou upando pro git");
