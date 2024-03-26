1d4b8ee515f92f0482a24f78dc9325f1bcfd4c85
LucasDevSystem <highspeedsetup@gmail.com>
2024-03-26 08:28:57 -0300

    First commit

---
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -0,0 +1,13 @@
+package aula.git;
+
+public class Apresentacao {
+
+    public void apresentar() {
+        this.lucasSouza();
+    }
+
+    private void lucasSouza() {
+        System.out.println("");
+        System.out.println("Estou colaborando com o git");
+    }
+}
---
+++ aula/src/main/java/aula/git/Main.java
@@ -0,0 +1,8 @@
+package aula.git;
+
+public class Main {
+    public static void main(String[] args) {
+        Apresentacao apresentacao = new Apresentacao();
+        apresentacao.apresentar();
+    }
+}
